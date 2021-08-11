3fba5afbaaaa9e3695ce23f81b652900e7364be3
Shrijhaa <78959087+Shrijhaa@users.noreply.github.com>
2021-08-11 16:29:22 +0530

    Add files via upload

---
+++ init.js
@@ -0,0 +1,18 @@
+
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
+
+/*
+
+{
+  status: 'connected',
+  authResponse: {
+      accessToken: '...',
+      expiresIn:'...',
+      signedRequest:'...',
+      userID:'...'
+  }
+}
+
+*/
