a983243d3eb63b31505d8c434223ed82de8d48ac
Shrijhaa <78959087+Shrijhaa@users.noreply.github.com>
2021-08-19 17:25:08 +0530

    Update google.js

--- google.js
+++ google.js
@@ -36,7 +36,7 @@ function renderButton() {
     gapi.signin2.render('my-signin2', {
         'scope': 'profile email',
         'width': 240,
-        'height': 50,
+        'height': 40,
         'longtitle': true,
         'theme': 'dark',
         'onsuccess': onSuccess,
